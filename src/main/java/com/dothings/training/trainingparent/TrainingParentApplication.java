07f34b4df48fcb3a5033a85ad272d5a3e769f992
mackson <1430665712@qq.com>
2018-12-07 15:54:05 +0800

    first commit

---
+++ src/main/java/com/dothings/training/trainingparent/TrainingParentApplication.java
@@ -0,0 +1,12 @@
+package com.dothings.training.trainingparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingParentApplication.class, args);
+	}
+}
